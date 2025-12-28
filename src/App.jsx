a21f3940d09acb0d41159e40565961f73b51a0d1
Wallison Dias <wallison.dias.711@ufrn.edu.br>
2025-12-28 17:45:22 -0300

    fix: ajeita adição de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,37 @@ function App() {
     }
   }
 
+  const toggleTask = (id) => {
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, completed : !task.completed } : task
+    ))
+  };
+ 
   return (
     <div className = "container">
       <h1>To-do List</h1>
+
       <div className = "input-box">
         <input
           type = "text"
           value = {inputText}
-          onChange = {(event) => setInputText(event.target)}
+          onChange = {(event) => setInputText(event.target.value)}
           placeholder = "What do you have to do?"
         />
         <button onClick = {addTask}>Adicionar</button>
       </div>
+
+      <ul className = "task-list">
+        {tasks.map(task => (
+          <li
+            key = {task.id}
+            className = {task.completed ? "completed" : ""}>
+              <span onClick={() => toggleTask(task.id)}>
+                {task.text}
+              </span>
+            </li>
+        ))}
+      </ul>
     </div>
   )
 }
