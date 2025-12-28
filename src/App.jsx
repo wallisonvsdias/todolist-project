78475e4bcbb5c191851934bbe7bea54f207abbbe
Wallison Dias <wallison.dias.711@ufrn.edu.br>
2025-12-28 18:14:41 -0300

    refactor: melhora a identação e legibilidade de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,55 +7,55 @@ function App() {
 
   const addTask = () => {
     if (inputText.trim() !== "") {
-    const newTask = {
-      id: Date.now(),
-      text: inputText,
-      completed: false
-    };
+      const newTask = {
+        id: Date.now(),
+        text: inputText,
+        completed: false,
+      };
 
-    setTasks([...tasks, newTask]);
-    setInputText('');
+      setTasks([...tasks, newTask]);
+      setInputText("");
     }
   }
 
   const toggleTask = (id) => {
-    setTasks(tasks.map(task =>
-      task.id === id ? { ...task, completed : !task.completed } : task
-    ))
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed : !task.completed } : task
+      )
+    )
   };
  
   const deleteTask = (id) => {
-    setTasks(tasks.filter(task => task.id !== id));
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
   return (
-    <div className = "container">
+    <div className="container">
       <h1>To-do List</h1>
 
-      <div className = "input-box">
+      <div className="input-box">
         <input
-          type = "text"
-          value = {inputText}
-          onChange = {(event) => setInputText(event.target.value)}
-          placeholder = "What do you have to do?"
+          type="text"
+          value={inputText}
+          onChange={(event) => setInputText(event.target.value)}
+          placeholder="What do you have to do?"
         />
-        <button onClick = {addTask}>Adicionar</button>
+        <button onClick={addTask}>
+          Adicionar
+        </button>
       </div>
 
-      <ul className = "task-list">
+      <ul className="task-list">
         {tasks.map(task => (
-          <li
-            key = {task.id}
-            className = {task.completed ? "completed" : ""}>
-              <span onClick={() => toggleTask(task.id)}>
-                {task.text}
-              </span>
-              <button
-                className = "delete-btn"
-                onClick = {() => deleteTask(task.id)}>
-                  Delete
-                </button>
-            </li>
+          <li key = {task.id} className = {task.completed ? "completed" : ""}>
+            <span onClick={() => toggleTask(task.id)}>
+              {task.text}
+            </span>
+            <button className = "delete-btn" onClick = {() => deleteTask(task.id)}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
